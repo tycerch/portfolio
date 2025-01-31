7c77ea3e4c4f32de2258a20c15636bff1b340ea8
Chris Tycer <ctycer@gmail.com>
2025-01-31 13:46:14 -0800

    Add SocialLinks component for GitHub, LinkedIn, and email navigation

---
+++ app/components/SocialLinks.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
+interface SocialLink {
+  icon: React.ReactNode;
+  url: string;
+}
+
+export default function SocialLinks() {
+  const links: SocialLink[] = [
+    { icon: <FaGithub />, url: "https://github.com/tycerch" },
+    { icon: <FaLinkedin />, url: "https://linkedin.com/in/chris-tycer-632b9796" },
+    { icon: <FaEnvelope />, url: "mailto:chris@tycer.dev" },
+  ];
+
+  return (
+    <aside className="hidden md:flex flex-col items-center fixed bottom-0 left-8 z-50">
+      <ul className="flex flex-col space-y-4 after:content-[''] after:w-[1px] after:h-24 after:bg-foreground after:mx-auto">
+        {links.map((link, idx) => (
+          <li key={idx}>
+            <Link
+              href={link.url}
+              target="_blank"
+              rel="noreferrer"
+              className="text-xl text-foreground hover:text-highlight transition-colors"
+            >
+              {link.icon}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
